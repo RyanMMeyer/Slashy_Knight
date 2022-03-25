1029531a5ea69cf5f3fee59f86bd1e1dfefebf74
RyanMMeyer <101270902+RyanMMeyer@users.noreply.github.com>
2022-03-25 08:38:58 -0500

    Added the Knight Sprite

--- Slashy Knight/Assets/Scripts/Knight.cs
+++ Slashy Knight/Assets/Scripts/Knight.cs
@@ -16,19 +16,19 @@ public class Knight : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.up * 5 * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.up * Time.deltaTime, Space.World);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.down * 5 * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.down * Time.deltaTime, Space.World);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * 5 * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.left * Time.deltaTime, Space.World);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * 5 * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.right * Time.deltaTime, Space.World);
         }
     }
 }
