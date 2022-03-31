a5c3031fa697b70c9d71b9c3a5463ea1f5d1e0c9
RyanMMeyer <101270902+RyanMMeyer@users.noreply.github.com>
2022-03-31 09:24:06 -0500

    Starting Collision Detection and Map Design

--- Slashy Knight/Assets/Scripts/Knight.cs
+++ Slashy Knight/Assets/Scripts/Knight.cs
@@ -5,30 +5,45 @@ using UnityEngine;
 public class Knight : MonoBehaviour
 {
     public int coins;
+    private RaycastHit2D hit;
+    private Vector3 moveDelta;
+    private BoxCollider2D boxCollider;
     // Start is called before the first frame update
     void Start()
     {
-        coins = 0;  
+        boxCollider = GetComponent<BoxCollider2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        
+    }
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveDelta = new Vector3(x, y, 0);
+
+        //if (moveDelta.x > 0)
+        //{
+        //    transform.localScale = Vector3.one;
+        //}
+        //else if (moveDelta.x < 0)
+        //{
+        //    transform.localScale = new Vector3(-1, 1, 1);
+        //}
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
         {
-            transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
         }
-        if (Input.GetKey(KeyCode.S))
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
         {
-            transform.Translate(Vector3.down * Time.deltaTime, Space.World);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * Time.deltaTime, Space.World);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * Time.deltaTime, Space.World);
+            transform.Translate(moveDelta.x * Time.deltaTime,0 , 0);
         }
     }
 }
