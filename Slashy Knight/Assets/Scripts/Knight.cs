bf875ca88d146528e4aca2a684da9607afa4dd8e
RyanMMeyer <101270902+RyanMMeyer@users.noreply.github.com>
2022-04-04 09:10:10 -0500

    Map Design and Collision Detection

--- Slashy Knight/Assets/Scripts/Knight.cs
+++ Slashy Knight/Assets/Scripts/Knight.cs
@@ -8,17 +8,13 @@ public class Knight : MonoBehaviour
     private RaycastHit2D hit;
     private Vector3 moveDelta;
     private BoxCollider2D boxCollider;
+
     // Start is called before the first frame update
     void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>(); 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
@@ -26,14 +22,6 @@ public class Knight : MonoBehaviour
 
         moveDelta = new Vector3(x, y, 0);
 
-        //if (moveDelta.x > 0)
-        //{
-        //    transform.localScale = Vector3.one;
-        //}
-        //else if (moveDelta.x < 0)
-        //{
-        //    transform.localScale = new Vector3(-1, 1, 1);
-        //}
         hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
         if (hit.collider == null)
         {
@@ -43,7 +31,7 @@ public class Knight : MonoBehaviour
         hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
         if (hit.collider == null)
         {
-            transform.Translate(moveDelta.x * Time.deltaTime,0 , 0);
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
         }
     }
 }
