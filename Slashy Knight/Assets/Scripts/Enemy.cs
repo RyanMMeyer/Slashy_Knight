d61d9b582c0cd6ff98b893ae96e64729b05ee167
RyanMMeyer <101270902+RyanMMeyer@users.noreply.github.com>
2022-05-04 09:21:31 -0500

    Health UI and script almost finished, and Enemy script almost finished

--- Slashy Knight/Assets/Scripts/Enemy.cs
+++ Slashy Knight/Assets/Scripts/Enemy.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool chasting;
+    private bool collidingWithPlayer;
+    public GameObject Knight;
+    private Vector3 startingPosition;
 
-    // Update is called once per frame
-    void Update()
+    private BoxCollider2D hitBox;
+    public float health = 5.0f;
+    public void Update()
     {
-        
+        if (Vector2.Distance(Knight.transform.position, gameObject.transform.position) < 0.8f)
+        {
+            gameObject.transform.Translate((Knight.transform.position - gameObject.transform.position) * Time.deltaTime * 1.2f);
+        }
+        if (health == 0.0f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Sword")
+        {
+            health -= 1.0f;
+        }
     }
 }
--- Slashy Knight/Assets/Scripts/Sword.cs
+++ Slashy Knight/Assets/Scripts/Sword.cs
@@ -33,5 +33,11 @@ public class Sword : MonoBehaviour
     
 
     }
-
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Slime Enemy")
+        {
+            Debug.Log("HIT ENEMY");
+        }
+    }
 }
